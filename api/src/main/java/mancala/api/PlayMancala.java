8d0e3351ebb78879b30ae510ae958bd4f0aabb85
Glenn Mulder <gmulder@sogyo.nl>
2022-06-07 13:43:20 +0000

    Angular mvc netter maken

--- api/src/main/java/mancala/api/PlayMancala.java
+++ api/src/main/java/mancala/api/PlayMancala.java
@@ -1,30 +1,29 @@
 package mancala.api;
 
-import java.io.IOException;
 import jakarta.servlet.http.*;
-import jakarta.servlet.ServletException;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.*;
 
 import mancala.api.models.*;
-import mancala.domain.MancalaException;
-import mancala.domain.MancalaImpl;
+import mancala.domain.Playable;
 
 @Path("/play")
 public class PlayMancala {
     @POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response initialize(
-			@Context HttpServletRequest request, 
-			PlayInput input) {
-		
-        HttpSession session = request.getSession(false);
-        MancalaImpl mancala = (MancalaImpl)session.getAttribute("mancala");
-		String namePlayer1 = (String)session.getAttribute("player1");
-        String namePlayer2 = (String)session.getAttribute("player2");
-		mancala.playPit(input.getIndex());
-		var output = new Mancala(mancala, namePlayer1, namePlayer2);
-		return Response.status(200).entity(output).build();
-	}
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response play(
+            @Context HttpServletRequest request) {
+        // Retrieve HTTP session.
+
+        // Retrieve game.
+
+        // Play a pit.
+
+        // Use the game to create a DTO.
+        MancalaDTO output = null;
+
+        // Send DTO back in response.
+        return Response.status(200).entity(output).build();
+    }
 }
--- api/src/main/java/mancala/api/StartMancala.java
+++ api/src/main/java/mancala/api/StartMancala.java
@@ -1,33 +1,33 @@
 package mancala.api;
 
-import java.io.IOException;
 import jakarta.servlet.http.*;
-import jakarta.servlet.ServletException;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.*;
 
 import mancala.api.models.*;
-import mancala.domain.MancalaImpl;
+import mancala.domain.Playable;
 
 @Path("/start")
 public class StartMancala {
-    @POST
+	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response execute(
-			@Context HttpServletRequest request, 
-			StartMancalaModel model) {
+	public Response start(
+			@Context HttpServletRequest request,
+			PlayerInputDTO players) {
+		// Create HTTP session.
+		HttpSession session = request.getSession(true);
 
-        var mancala = new MancalaImpl();
-        PlayerModel player1 = model.getPlayer1();
-		PlayerModel player2 = model.getPlayer2();
-		
-        HttpSession session = request.getSession(true);
-        session.setAttribute("mancala", mancala);
-        session.setAttribute("player1", player1.getName());
-        session.setAttribute("player2", player2.getName());
+		// Initialize game.
+		Playable mancala = null;
 
-		var output = new Mancala(mancala, player1.getName(), player2.getName());
+		// Attach game to session.
+		session.setAttribute("mancala", mancala);
+
+		// Use the game to create a DTO.
+		var output = new MancalaDTO(mancala);
+
+		// Send DTO back in response.
 		return Response.status(200).entity(output).build();
 	}
 }
