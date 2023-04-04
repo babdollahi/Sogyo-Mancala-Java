935283ca98170ffb84c3833f643f2373ac9bc4bd
Banafsheh Abdollahi <babdollahi@sogyo.nl>
2023-04-04 08:52:57 +0200

    Break on code coverage under 60%

--- domain/src/test/java/mancala/domain/KalahaTest.java
+++ domain/src/test/java/mancala/domain/KalahaTest.java
@@ -9,22 +9,22 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class KalahaTest {
 
-    @ParameterizedTest
-    @ValueSource(ints = { 6, 13 })
-    public void testDeclareCorrectWinner(int kalahaIndex) {
-        Bowl firstBowl = new PlayableBowl(new int[] { 0, 0, 0, 0, 0, 0, 30, 0, 0, 0, 0, 0, 0, 18 });
-        Kalaha kalaha = (Kalaha) firstBowl.getBowlAtDistance(kalahaIndex);
-        Optional<Player> winner = kalaha.whoIsAhead();
-        assertEquals(Optional.of(firstBowl.owner), winner);
-    }
+    // @ParameterizedTest
+    // @ValueSource(ints = { 6, 13 })
+    // public void testDeclareCorrectWinner(int kalahaIndex) {
+    //     Bowl firstBowl = new PlayableBowl(new int[] { 0, 0, 0, 0, 0, 0, 30, 0, 0, 0, 0, 0, 0, 18 });
+    //     Kalaha kalaha = (Kalaha) firstBowl.getBowlAtDistance(kalahaIndex);
+    //     Optional<Player> winner = kalaha.whoIsAhead();
+    //     assertEquals(Optional.of(firstBowl.owner), winner);
+    // }
 
-    @ParameterizedTest
-    @ValueSource(ints = { 6, 13 })
-    public void testRecognizingADraw(int kalahaIndex) {
-        Bowl firstBowl = new PlayableBowl(new int[] { 0, 0, 0, 0, 0, 0, 24, 0, 0, 0, 0, 0, 0, 24 });
-        Kalaha kalaha = (Kalaha) firstBowl.getBowlAtDistance(kalahaIndex);
-        Optional<Player> winner = kalaha.whoIsAhead();
-        assertEquals(Optional.empty(), winner);
-    }
+    // @ParameterizedTest
+    // @ValueSource(ints = { 6, 13 })
+    // public void testRecognizingADraw(int kalahaIndex) {
+    //     Bowl firstBowl = new PlayableBowl(new int[] { 0, 0, 0, 0, 0, 0, 24, 0, 0, 0, 0, 0, 0, 24 });
+    //     Kalaha kalaha = (Kalaha) firstBowl.getBowlAtDistance(kalahaIndex);
+    //     Optional<Player> winner = kalaha.whoIsAhead();
+    //     assertEquals(Optional.empty(), winner);
+    // }
 
 }
--- domain/src/test/java/mancala/domain/PlayerTest.java
+++ domain/src/test/java/mancala/domain/PlayerTest.java
@@ -9,63 +9,63 @@ import org.junit.jupiter.api.Test;
 
 class PlayerTest {
 
-    @Test
-    public void testNewPlayerHasTurn() {
-        Player player = new Player();
-        assertTrue(player.hasTurn());
-    }
+    // @Test
+    // public void testNewPlayerHasTurn() {
+    //     Player player = new Player();
+    //     assertTrue(player.hasTurn());
+    // }
 
-    @Test
-    public void testOpponentOfNewPlayerDoesNotHaveTurn() {
-        Player player = new Player();
-        Player opponent = player.getOpponent();
-        assertFalse(opponent.hasTurn());
-    }
+    // @Test
+    // public void testOpponentOfNewPlayerDoesNotHaveTurn() {
+    //     Player player = new Player();
+    //     Player opponent = player.getOpponent();
+    //     assertFalse(opponent.hasTurn());
+    // }
 
-    @Test
-    public void testPlayerDoesNotEqualOpponent() {
-        Player player = new Player();
-        Player opponent = player.getOpponent();
-        assertNotEquals(player, opponent);
-    }
+    // @Test
+    // public void testPlayerDoesNotEqualOpponent() {
+    //     Player player = new Player();
+    //     Player opponent = player.getOpponent();
+    //     assertNotEquals(player, opponent);
+    // }
 
-    @Test
-    public void testOpponentOfOpponentIsPlayer() {
-        Player player = new Player();
-        Player opponent = player.getOpponent();
-        assertEquals(player, opponent.getOpponent());
-    }
+    // @Test
+    // public void testOpponentOfOpponentIsPlayer() {
+    //     Player player = new Player();
+    //     Player opponent = player.getOpponent();
+    //     assertEquals(player, opponent.getOpponent());
+    // }
 
-    @Test
-    public void testSwitchingTurnSwitchesTurn() {
-        Player player = new Player();
-        player.switchTurn();
-        assertFalse(player.hasTurn());
-        assertTrue(player.getOpponent().hasTurn());
-    }
+    // @Test
+    // public void testSwitchingTurnSwitchesTurn() {
+    //     Player player = new Player();
+    //     player.switchTurn();
+    //     assertFalse(player.hasTurn());
+    //     assertTrue(player.getOpponent().hasTurn());
+    // }
 
-    @Test
-    public void testSwitchingTurnOnOpponentAlsoSwitchesTurn() {
-        Player player = new Player();
-        player.getOpponent().switchTurn();
-        assertFalse(player.hasTurn());
-        assertTrue(player.getOpponent().hasTurn());
-    }
+    // @Test
+    // public void testSwitchingTurnOnOpponentAlsoSwitchesTurn() {
+    //     Player player = new Player();
+    //     player.getOpponent().switchTurn();
+    //     assertFalse(player.hasTurn());
+    //     assertTrue(player.getOpponent().hasTurn());
+    // }
 
-    @Test
-    public void testAfterGameOverNooneHasTurn() {
-        Player player = new Player();
-        player.gameOver();
-        assertFalse(player.hasTurn());
-        assertFalse(player.getOpponent().hasTurn());
-    }
+    // @Test
+    // public void testAfterGameOverNooneHasTurn() {
+    //     Player player = new Player();
+    //     player.gameOver();
+    //     assertFalse(player.hasTurn());
+    //     assertFalse(player.getOpponent().hasTurn());
+    // }
 
-    @Test
-    public void testAfterOpponentGameOverNooneHasTurn() {
-        Player player = new Player();
-        player.getOpponent().gameOver();
-        assertFalse(player.hasTurn());
-        assertFalse(player.getOpponent().hasTurn());
-    }
+    // @Test
+    // public void testAfterOpponentGameOverNooneHasTurn() {
+    //     Player player = new Player();
+    //     player.getOpponent().gameOver();
+    //     assertFalse(player.hasTurn());
+    //     assertFalse(player.getOpponent().hasTurn());
+    // }
 
 }
